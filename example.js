6e182b39b4500f79ef162ecf47ce74511d23219b
Henczidai Bálint <henczidaibalint@gmail.com>
2018-09-25 21:33:03 +0200

    array support

--- example.js
+++ example.js
@@ -1,31 +1,67 @@
 require('./typs.js');
 
-class B {
+class Foo {
 	constructor() {
-		this.x = '42'
-		this.logT = () => console.log('BT')
+		this.x = '24';
+	}
+	log(){
+		console.log('Foo');
+	}
+}
+
+class Bar {
+	constructor() {
+		this.x = '42';
 	}	
 	log(){
-		console.log('B')
+		console.log('Bar')
 	}
-}
-B.prototype.__types__ = {}
-
-class A{
-	constructor() {
-		this.x = '24'
-		this.logT = () => console.log('AT')
-	}
-	log(){
-		console.log('A');
-	}
-}
-A.prototype.__types__ = {
-	b: B,
-	a: A,
 }
 
-var parsed = TYPS.parse({b:{x:11, y:99}, a:{b:{}}, x:10, y:66}, A);
+class Baz {
+    log() {
+        console.log('Baz')
+    }
+}
+
+
+Foo.__types__ = {
+	foo: Foo,
+    bar: Bar,
+    arrFoo: Foo,
+    arrBar: Bar,
+    baz: Baz,
+}
+
+Bar.__types__ = {
+    foo: Foo,
+    arrFoo: Foo,
+}
+
+var parsed = TYPS.parse({
+    foo: { x: 1, y: 2 },
+    bar: { z: 3 },
+    baz: { w: 4 },
+    arrFoo: [{ x: 10, y:20 }, { x: 11, y: 21 }, {}],
+    arrBar: [
+        { 
+            foo: { x: 100, y:200 },
+            arrFoo: [{ x: 1000, y:2000 }, {}] 
+        },
+        {},
+    ],
+    obj: {
+        subObj: {
+            str: 'test'
+        },
+        num: 99,
+        bool: true,
+        null: null,
+    },
+    x: 0,
+    y: 0,
+}, Foo);
+
 console.log('parse:');
 console.log(parsed);
 console.log('stringify:');
--- typs.js
+++ typs.js
@@ -2,23 +2,31 @@
  * TYPS.js
  */
 (function(global){
-	function reparse(obj, json){
-		for (var key in json) {
-			if (!!obj.__types__ && !!obj.__types__[key]) {
-				obj[key] = reparse(new obj.__types__[key], json[key]);
-			} else {
-				obj[key] = json[key];
-			} 
-		} 
-		return obj;
+	function reparse(type, json){
+        if (!type) return json;
+        var ret, t = Object.prototype.toString.call(json);
+        if (t == '[object Array]') {
+            ret = [];
+            for (var i = 0; i < json.length; i++) {
+                ret.push(reparse(type, json[i]));
+            }
+        } else if(t == '[object Object]'){
+            ret = new type;
+            for (var key in json) {
+                var subType = (type.__types__ && type.__types__[key]) || null;
+                ret[key] = reparse(subType, json[key]);
+            }
+        } else {
+            ret = json;
+        }
+		return ret;
 	}
 	global.TYPS = {
 		parse: function(json, rootType) {
 			if(typeof(json) == "string"){
 				json = JSON.parse(json);
 			}
-			if(!rootType) return json;
-			return reparse(new rootType, json);
+			return reparse(rootType, json);
 		},
 		stringify: function(...args){
 			return JSON.stringify(...args);
